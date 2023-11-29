bf8c9b19197c6a923e18ac110c2c3fbaad3de798
Joseph <joseph.dena@student.moringaschool.com>
2023-11-29 22:15:36 +0300

    Completed Assignment

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,25 @@
-// Code EyesOnMe Component Here
+import React from 'react';
+
+const EyesOnMe = () => {
+  const handleFocus = () => {
+    console.log('Good!');
+  };
+
+  const handleBlur = () => {
+    console.log('Hey! Eyes on me!');
+  };
+
+  return (
+    <div>
+      <button
+        text="Eyes on me"
+        onFocus={handleFocus}
+        onBlur={handleBlur}
+      >
+        Eyes on me
+      </button>
+    </div>
+  );
+};
+
+export default EyesOnMe;
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,9 +1,19 @@
-// Code Keypad Component Here
+import React from 'react';
 
-function Keypad (){
-    return (
-        <div></div>
-    )
-}
+const Keypad = () => {
+  const handleChange = (e) => {
+    console.log('Entering password...');
+  };
+
+  return (
+    <div>
+      <input
+        type="password"
+        onChange={handleChange}
+        placeholder="Enter Password"
+      />
+    </div>
+  );
+};
 
 export default Keypad;
